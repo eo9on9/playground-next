f557c0f8bfa8dc38e371adde5370c97fff640dfd
eo9on9 <eo9on9@gmail.com>
2025-10-17 12:39:07 +0900

    chore: set jest

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }],
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+}
